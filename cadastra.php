1af8db5ed476247b2963785a1b271ff9f4f42864
VicMartins <vic.martins0907@gmail.com>
2019-10-01 15:15:58 -0300

    Arquivo

---
+++ cadastra.php
@@ -0,0 +1,15 @@
+<?php
+
+  $conexao = mysqli_connect("localhost","id10996549_apppizza","bananaazul2405","id10996549_apppizza");
+ //                    onde está o banco, usuario banco,           senha ,     nome do banco de dados
+
+  $sabor = $_POST['sabor'];
+  $valor = $_POST['valor'];
+
+  $query = "insert into tb_pizza values (null,'$sabor',$valor')";
+
+  mysqli_query($conexao,$query);
+
+  echo "Registro salvo com sucesso!";
+
+   ?>
