5ccd0719f1bbf2d27b4e9e685a8e34cc6097e0ae
Victória Martins <49958650+VicMartins@users.noreply.github.com>
2019-10-10 14:24:55 -0300

    Update cadastra.php

--- cadastra.php
+++ cadastra.php
@@ -6,7 +6,7 @@
   $sabor = $_POST['sabor'];
   $valor = $_POST['valor'];
 
-  $query = "insert into tb_pizza values (null,'$sabor',$valor')";
+  $query = "insert into tb_pizza values (null,'$sabor',$valor)";
 
   mysqli_query($conexao,$query);
 
